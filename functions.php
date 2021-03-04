3b328ed1bffb6fcf8fcfc9f376d9c8b09ff14502
Nic Lindh <niclas.lindh@asu.edu>
2021-03-04 16:00:47 -0700

    make acf sync work in child theme

--- functions.php
+++ functions.php
@@ -1,17 +1,26 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
-add_action( 'wp_enqueue_scripts', 'uds_wordpress_child_scripts' );
-function uds_wordpress_child_scripts() {
+add_filter('acf/settings/load_json', 'parent_theme_field_groups');
+function parent_theme_field_groups($paths)
+{
+    $path = get_template_directory() . '/acf-json';
+    $paths[] = $path;
+    return $paths;
+}
+
+add_action('wp_enqueue_scripts', 'uds_wordpress_child_scripts');
+function uds_wordpress_child_scripts()
+{
     // Get the theme data.
-    $the_theme     = wp_get_theme();
-    $theme_version = $the_theme->get( 'Version' );
+    $the_theme = wp_get_theme();
+    $theme_version = $the_theme->get('Version');
 
-    $css_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/css/child-theme.min.css' );
-    wp_enqueue_style( 'uds-wordpress-child-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array('uds-wordpress-styles'), $css_child_version );
+    $css_child_version = $theme_version . '.' . filemtime(get_stylesheet_directory() . '/css/child-theme.min.css');
+    wp_enqueue_style('uds-wordpress-child-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array('uds-wordpress-styles'), $css_child_version);
 
-    $js_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/js/child-theme.min.js' );
-    wp_enqueue_style( 'uds-wordpress-child-styles', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array('jquery'), $js_child_version );
+    $js_child_version = $theme_version . '.' . filemtime(get_stylesheet_directory() . '/js/child-theme.min.js');
+    wp_enqueue_style('uds-wordpress-child-styles', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array('jquery'), $js_child_version);
 }
